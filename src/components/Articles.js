9249cf0ac4a6d2034c95d433997ffbf04398ae45
Ryan Pearson <ryanpearson015@gmail.com>
2021-11-14 15:02:29 -0600

    added commented use effect into App function in app.js file, made some edits to map in articles file. Still not working correctly

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,13 +1,29 @@
 import React  from 'react'
 //import Article from './Article'
 
- const Articles = (props) => {
-   console.log("articleData in Articles: ", props.articleData)
-    return (
-      <div>
+export default function Articles (props) {
+  return (
+    <div>
+    <ul>
+      {props.articleData.map((article, index) => {
+          return (
+              <li className="list">
+                  <h3>{props.article.hits.title}<a href={props.article.hits.url} id={"url"}>{` (${props.article.hits.url})`}</a></h3>
+              </li>
+          )
+      })}
+      </ul>
+      </div>
+      )
+}
+
+
+
+ 
+/*
+      
         -
-        {
-          //I think this is what we need to use inside of the map function to get the articles to show up on the actual screen
+        //I think this is what we need to use inside of the map function to get the articles to show up on the actual screen
           //props.articleData.hits.author
           //props.articleData.hits.title
           //props.articleData.hits.url
@@ -16,16 +32,17 @@ import React  from 'react'
           //this map is coming up as props.articleData.map is not a function when i hit the search bar
           //in all the previous assignments i never had an issue with this, I think the issue is how i named it
           //but im not sure what the correct way to name it is
-        /* props.articleData.map(Article => (
-          <div className="story-container" key={Article.objectID}>
-            <h4 style={{margin: "5px"}}>{Article.title}</h4>
-            <p style={{fontSize: "20px", margin: "5px"}}>Author: {Article.author}</p>
-            <a href={Article.url} target="_blank" rel="noreferrer" style={{color: "white", fontSize: "18px"}}>Visit Website</a>
+        <ul className='Articles'>
+        {props.articleData.map((articleData) => (
+          <div className="story-container" key={props.articleData.objectID}>
+            <h4 style={{margin: "5px"}}>{props.articleData.hits.title}</h4>
+            <p style={{fontSize: "20px", margin: "5px"}}>Author: {props.articleData.hits.author}</p>
+            <a href={props.articleData.hits.url} target="_blank" rel="noreferrer" style={{color: "white", fontSize: "18px"}}>Visit Website</a>
           </div>
-        ))} */}
-      </div>
-    )
-  }
+        ))}
+      
+    
+      </ul>
+    )*/
 
-  export default Articles;
 
