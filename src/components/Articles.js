516e0ba051924b30d603d2604650b8d457b5e7df
Ryan Pearson <ryanpearson015@gmail.com>
2021-11-16 16:59:06 -0600

    added styling

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,17 +1,21 @@
+
 import React  from 'react'
-import Article from './Article'
+//import Article from './Article'
 
-export default function Articles(props) {
-    return (
-        <div>
-        <ul>
-            {props.articles.map((article) => {
 
-                return(
-                    <Article article={article}/>
-                )
-            })}
-        </ul>
-        </div>
-    )
+export default function Articles (props) {
+  return (
+    <div>
+    <ul>
+      {props.articleData.map((article, index) => {
+          return (
+              <li className="list" style ={ index % 2? { background : "silver" }:{ background : "gainsboro" }}>
+                  <h5>{article.title}  <h4>Author: {article.author}</h4>  <a href={article.url} id={"url"}>{` (${article.url})`}</a>   </h5>
+                  
+              </li>
+          )
+      })}
+      </ul>
+      </div>
+      )
 }
